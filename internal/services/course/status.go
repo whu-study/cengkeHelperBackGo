514e81f56673fb14255aa56adcc2ea1063d0d8c1
fordecember <1921567337@qq.com>
2025-11-18 17:51:37 +0800

    fix: 匹配今日课程数

--- internal/services/course/status.go
+++ internal/services/course/status.go
@@ -16,7 +16,8 @@ func getNumOfCourses(dayOfWeek, weekNum int, lessonNum []int) int {
 		 FROM time_infos ti 
 		 JOIN course_infos ci ON ci.id = ti.course_info_id
 		 WHERE ti.day_of_week = ? 
-		   AND ti.week_and_time & ? = ?`,
+           AND (ti.week_and_time & ? & (-1<<13)) != 0
+           AND (ti.week_and_time & ? & 0x1FFF) != 0`,
 		dayOfWeek, weekAndTime, weekAndTime,
 	).Scan(&count).Error; err != nil {
 		fmt.Println(err)
@@ -32,7 +33,7 @@ func GetSingleNumOfCourses(dayOfWeek, weekNum int, lessonNum int) int {
 func GetOneDayNumOfCourses(dayOfWeek, weekNum int) int {
 	lessonNums := make([]int, 13)
 	for i := 1; i <= 13; i++ {
-		lessonNums = append(lessonNums, i)
+		lessonNums[i-1] = i
 	}
 	return getNumOfCourses(dayOfWeek, weekNum, lessonNums)
 }
